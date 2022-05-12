8e3e17a2c01047750fe94e1e40c6ff3575d8832c
Tahsin Ahmed Chowdhury <39807939+tchowdhu@users.noreply.github.com>
2022-05-12 09:46:11 -0500

    Update model_driven_programming.py

--- model_driven_programming.py
+++ model_driven_programming.py
@@ -1,3 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Python example script showing proper use of the Cisco Sample Code header.
+Copyright (c) 2022 Cisco and/or its affiliates.
+This software is licensed to you under the terms of the Cisco Sample
+Code License, Version 1.1 (the "License"). You may obtain a copy of the
+License at
+               https://developer.cisco.com/docs/licenses
+All use of the material herein must be in accordance with the terms of
+the License. All rights not expressly granted by the License are
+reserved. Unless required by applicable law or agreed to separately in
+writing, software distributed under the License is distributed on an "AS
+IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+or implied.
+"""
+
+
+from __future__ import absolute_import, division, print_function
+
+
+__author__ = "Tahsin Chowdhury <tchowdhu@cisco.com>"
+__contributors__ = [
+    "Tahsin Chowdhury <tchowdhu@cisco.com>"
+]
+__copyright__ = "Copyright (c) {{current_year}} Cisco and/or its affiliates."
+__license__ = "Cisco Sample Code License, Version 1.1"
+
+
+# Your code goes here.
+indent = 4
+print(
+    __doc__,
+    "Author:",
+    " " * indent + __author__,
+    "Contributors:",
+    "\n".join([" " * indent + name for name in __contributors__]),
+    "",
+    __copyright__,
+    "Licensed Under: " + __license__,
+    sep="\n"
+)
+
 from pprint import pprint
 
 import xmltodict as xmltodict
